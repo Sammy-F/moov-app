bd2b9e4ce4d3cbe959fbcc6e8eeab3ce17c3eb19
Sammy-F <sfritsch@macalester.edu>
2018-02-16 14:47:32 -0600

    Added logout button to placeholder page

--- app/src/main/java/app/moov/moov/Feed.java
+++ app/src/main/java/app/moov/moov/Feed.java
@@ -1,13 +1,42 @@
 package app.moov.moov;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Feed extends AppCompatActivity {
+
+    private Button btnLogout;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        setUIViews();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+    }
+
+    private void logout() {
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(Feed.this, MainActivity.class));
+    }
+
+    private void setUIViews(){
+        btnLogout = (Button) findViewById(R.id.btnLogout);
     }
 }
