7ac3e9e73ced9d9e5b51e41125509d8df2c0b7aa
Sammy-F <sfritsch@macalester.edu>
2018-02-15 21:04:42 -0600

    Began building prettier login screen under activity name MainActivity

--- app/src/main/java/app/moov/moov/MainActivity.java
+++ app/src/main/java/app/moov/moov/MainActivity.java
@@ -2,8 +2,14 @@ package app.moov.moov;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
