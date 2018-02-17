7fe3c3786aab5376e371bff7ea9279bc04171905
Sammy-F <sfritsch@macalester.edu>
2018-02-17 16:20:35 -0600

    built super basic feed adapter

--- app/src/main/java/app/moov/moov/Feed.java
+++ app/src/main/java/app/moov/moov/Feed.java
@@ -3,11 +3,14 @@ package app.moov.moov;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class Feed extends AppCompatActivity {
 
@@ -15,6 +18,10 @@ public class Feed extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
 
     private RecyclerView rvFeed;
+    private RecyclerView.Adapter feedAdapter;
+    private RecyclerView.LayoutManager feedManager;
+
+    private ArrayList<String> testStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,23 @@ public class Feed extends AppCompatActivity {
     }
 
     private void setUIViews(){
+
+        testStrings.add("lorem");
+        testStrings.add("ipsum");
+        testStrings.add("placeholder");
+
         btnLogout = (Button) findViewById(R.id.btnLogout);
         rvFeed = (RecyclerView) findViewById(R.id.rvFeed);
+
+        //improve performance if all posts have fixed size
+        rvFeed.hasFixedSize();
+
+        //set recycler view manager
+        feedManager = new LinearLayoutManager(this);
+        rvFeed.setLayoutManager(feedManager);
+
+        //specify adapter
+        feedAdapter = new FeedAdapter(testStrings);
+
     }
 }
