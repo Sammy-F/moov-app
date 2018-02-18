13ff64f66cecdee17eb2d3e6f7fe2e51e623a6c0
shirai21791 <lisashirai0104@gmail.com>
2018-02-17 20:26:09 -0600

    Created a FeedPage Created a new review activity

--- app/src/main/java/app/moov/moov/PostActivity.java
+++ app/src/main/java/app/moov/moov/PostActivity.java
@@ -9,5 +9,6 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        //I am a comment
     }
 }
