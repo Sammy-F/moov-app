600248f3842ffd21a2fa205bf879208bfefbef46
shirai21791 <lisashirai0104@gmail.com>
2018-03-24 20:34:28 -0500

    Added User Class

---
+++ app/src/main/java/app/moov/moov/User.java
@@ -0,0 +1,19 @@
+package app.moov.moov;
+
+/**
+ * Created by Lisa on 24/03/18.
+ */
+
+public class User {
+    public String username;
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
