d2d7d49c2c42ca4b4253368f281af4c461455aca
Sammy-F <sfritsch@macalester.edu>
2018-03-29 23:31:29 -0500

    implemented password changing

--- app/src/main/java/app/moov/moov/activity/UserSettingsActivity.java
+++ app/src/main/java/app/moov/moov/activity/UserSettingsActivity.java
@@ -1,16 +1,21 @@
 package app.moov.moov.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 
 import app.moov.moov.R;
 
 public class UserSettingsActivity extends AppCompatActivity {
+
+    Button btnChangePass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,20 @@ public class UserSettingsActivity extends AppCompatActivity {
 
     private void setUIViews() {
 
+        btnChangePass = (Button) findViewById(R.id.btnChangePassword);
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Class destination = ChangePasswordActivity.class;
+                btnClick(destination);
+            }
+        });
+
+    }
+
+    private void btnClick(Class destination) {
+        Intent intent = new Intent(UserSettingsActivity.this, destination);
+        startActivity(intent);
     }
 
 }
