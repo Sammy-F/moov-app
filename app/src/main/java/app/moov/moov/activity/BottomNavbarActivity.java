1ef8e82fc9b9b493a3a21422a274db83da1a1649
shirai21791 <lisashirai0104@gmail.com>
2018-03-31 23:26:23 -0500

    added bottom nav bar stuff for experimentation

---
+++ app/src/main/java/app/moov/moov/activity/BottomNavbarActivity.java
@@ -0,0 +1,8 @@
+package app.moov.moov.activity;
+
+/**
+ * Created by Lisa on 31/03/18.
+ */
+
+public class BottomNavbarActivity {
+}
---
+++ app/src/main/java/app/moov/moov/activity/NavBar.java
@@ -0,0 +1,10 @@
+package app.moov.moov.activity;
+
+/**
+ * Created by Lisa on 31/03/18.
+ */
+
+public class NavBar {
+
+
+}
