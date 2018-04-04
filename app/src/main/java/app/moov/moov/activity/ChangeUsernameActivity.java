c36e16d686c3acd4f036be4ba76ca3cb40bf94b2
shirai21791 <lisashirai0104@gmail.com>
2018-04-04 17:48:25 -0500

    Created ChangeUsernameActivity class

---
+++ app/src/main/java/app/moov/moov/activity/ChangeUsernameActivity.java
@@ -0,0 +1,79 @@
+package app.moov.moov.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import app.moov.moov.R;
+
+/**
+ * Created by Lisa on 04/04/18.
+ */
+
+public class ChangeUsernameActivity extends ToolbarBaseActivity{
+
+    private EditText etCurrentUserName;
+    private EditText etNewUserName;
+    private Button btnChangeUsername;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_change_email);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolBarSetup(toolbar);
+        BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.navBar);
+        setUpNavBar(navBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        setUIViews();
+    }
+
+    private void setUIViews() {
+        etCurrentUserName = (EditText) findViewById(R.id.etEmail); ///CHANGE THIS
+        etNewUserName = (EditText) findViewById(R.id.etPass); ///CHANGE THIS
+
+        btnChangeUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                changeUsername();
+            }
+        });
+
+        findViewById(R.id.constraintLayout).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
+    }
+
+    private void changeEmail() {
+
+
+    }
+
+}
